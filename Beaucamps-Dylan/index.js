dd93fc70f907898028eeced42a5473214fb63304
Dylan <beaucamps.dylan@gmail.com>
2016-11-14 15:23:30 +0100

    dylan

---
+++ Beaucamps-Dylan/index.js
@@ -0,0 +1,15 @@
+/*function myFunction(div) {
+	div.style.background="red";
+	}*/
+$('#red').click(function(){
+	console.log('click');
+	$(this).toggleClass('rouge');
+});
+$('#red2').click(function(){
+	console.log('click');
+	$(this).toggleClass('rouge');
+});
+$('#red1').click(function(){
+	console.log('click');
+	$(this).toggleClass('rouge');
+});
