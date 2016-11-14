a8aab5704108469689aa1c7212f9ef7037a8f62e
beenie <abdoulayedssou@gmail.com>
2016-11-14 17:12:05 +0100

    beenie-man

---
+++ Abdoulaye-Sacko/index.js
@@ -0,0 +1,15 @@
+/*function color_change(){
+ 
+ 
+document.getElementByClassName("color_change item").style.background= "red";
+
+
+
+};
+*/
+
+
+function myFunction(divObj) {
+    divObj.style.background="red";
+
+}
