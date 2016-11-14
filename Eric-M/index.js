7014ab49b1bfad51d64f154cf3711487303df471
MBAIRAMADJI <mbairamadjieric@gmail.com>
2016-11-14 17:23:41 +0100

    commentaire js

--- Eric-M/index.js
+++ Eric-M/index.js
@@ -13,7 +13,7 @@ box[2].onclick = function (){
 	box[2].style.backgroundColor = "red";
 }
 
-/*
+/* faire avec une boucle
 	for(var i=0; i < box.length; i++){
  	box[i].onclick = function () {
  		box[i].style.backgroundColor = "red";
