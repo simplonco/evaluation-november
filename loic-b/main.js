752d0b964e18cccd0e8cf51c967b61a9312534cd
Loic <Loic@Mac-111.local>
2016-11-14 11:41:37 +0100

    Loic

---
+++ loic-b/main.js
@@ -0,0 +1,18 @@
+// var bloc1 = document.getElementById('Bloc1');
+// var bloc2 = document.getElementById('Bloc2');
+// var bloc3 = document.getElementById('Bloc3');
+// //
+// // style.backgroundColor = "red";
+//
+// function myFunction1() {
+//   bloc1.style.backgroundColor = "yellow";
+// }
+// function myFunction2() {
+//   bloc2.style.backgroundColor = "yellow";
+// }
+// function myFunction3() {
+//   bloc3.style.backgroundColor = "yellow";
+// }
+function myFunction(Holla) {
+  Holla.style.backgroundColor = "red";
+}
