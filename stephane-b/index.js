73a4712ac6bd30022394acfb8a1cbb97ce2f1dba
stephane.b <stewane90@gmail.como>
2016-11-22 16:20:30 +0100

    stephane bouchene

---
+++ stephane-b/index.js
@@ -0,0 +1,14 @@
+var menu1= document.getElementById("menu1");
+menu1.onclick = function (){
+	menu1.style.backgroundColor = "red";
+}
+
+var menu2= document.getElementById("menu2");
+menu2.onclick = function (){
+	menu2.style.backgroundColor = "red";
+}
+
+var menu3= document.getElementById("menu3");
+menu3.onclick = function (){
+	menu3.style.backgroundColor = "red";
+}
